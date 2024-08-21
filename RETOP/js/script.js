a6185c2d9c4a07101f559ade5310e97067d56c29
Isacc Gosme Palacios <143350307+Isaccr7@users.noreply.github.com>
2024-08-20 22:03:43 -0500

    Add files via upload

---
+++ RETOP/js/script.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById('noBtn');
+
+noBtn.addEventListener('mouseover', () => {
+    // Genera una posición aleatoria dentro de los límites de la ventana
+    const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
+
+document.getElementById('yesBtn').addEventListener('click', () => {
+    window.location.href = 'flowers.html';  // Redirige a la página flowers.html
+});
